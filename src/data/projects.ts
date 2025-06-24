53bcbd63928cc3196efde528215e63c3c33e6e21
Catrin Tornqvist <catrin.tq@gmail.com>
2025-06-24 15:32:38 +0200

    fix img path for third time

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,7 +5,7 @@ export const projects = [
     type: "Collab Vallentuna Bibliotek",
     description:
     "A web application designed for children and youth to keep track reading projects in a simple and fun way, developed as a prototype for a friend. The app features a lightweight backend built with Express and MongoDB, and a responsive frontend developed using Vue and sass.",
-    image: "/img/book.jpg",
+    image: "img/book.jpg",
     link: "https://example.com",
     tags: ["Vue", "Sass", "TypeScript", "MongoDB", "Express"],
   },
@@ -15,7 +15,7 @@ export const projects = [
     type: "School project",
     description:
       "A fully responsive e-commerce project built with Vue and TypeScrip. Images are generated with an AI image generator and functions are developed based on a school assignment, with functions for the cart, highlights when adding a new item, a form for purchase and much more.",
-    image: "/img/donut.jpg",
+    image: "img/donut.jpg",
     link: "https://catrintq.github.io/donut-shop/",
     tags: ["React", "Sass", "TypeScript"],
   },
@@ -25,7 +25,7 @@ export const projects = [
     type: "School project",
     description:
       "A lightweight REST API for a basic webshop, built with TypeScript and Express.js. The backend runs on a local XAMPP server and uses MySQLfor data storage. The API supports full CRUD operations for managing products and categories, including fields like name, price,description, and stock status.",
-    image: "/img/api.jpg",
+    image: "img/api.jpg",
     link: "https://example.com",
     tags: ["MariaDB", "XAMPP", "TypeScript", "Express.js", "MYSQL"],
   },
@@ -35,7 +35,7 @@ export const projects = [
     type: "School project",
     description:
       "A responsive Todo app built with React and TypeScript. Users can add, remove, and sort todos, as well as mark tasks as done or undone. Todos are saved in localStorage to persist between sessions. The app features priority selection, a clean UI using Tailwind CSS and dark mode toggle (in progress).",
-    image: "/img/todo.jpg",
+    image: "img/todo.jpg",
     link: "https://example.com",
     tags: ["React", "Tailwind", "TypeScript"],
   },
